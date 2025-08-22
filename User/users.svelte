586dfd2e9c52a60f95b7e0f81025297c0a09f40a
golfang01 <117799607+golfang01@users.noreply.github.com>
2025-08-22 17:03:31 +0700

    Add files via upload

---
+++ User/users.svelte
@@ -0,0 +1,27 @@
+<script>
+    let users = [
+      { id: 1, name: 'User A', status: 'Active' },
+      { id: 2, name: 'User B', status: 'Inactive' },
+    ];
+  </script>
+  
+  <main>
+    <h1>Users</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each users as user}
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.status}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </main>
+  
