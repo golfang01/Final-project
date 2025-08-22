7e3b01ae694d1fc6a090bd5aa132256be5901881
golfang01 <117799607+golfang01@users.noreply.github.com>
2025-08-22 16:58:20 +0700

    Add files via upload

---
+++ login/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let data;
+</script>
+
+<main class="auth-layout">
+  <slot />
+</main>
+
+<style>
+  .auth-layout {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: #f5f5f5;
+  }
+</style>
